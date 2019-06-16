889c86fa4f2447bba2a010b7782098f02f1f5c08
suchov <artem.suchov@gmail.com>
2019-06-16 20:17:15 +0300

    code tests for congrats component

---
+++ src/Congrats.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @returns {JSX.Element} - Render component (or null if `success` props is false)
+ */
+
+export default () => {
+  return <div />;
+};
---
+++ src/Congrats.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+
+import { findByTestAttr } from "../test/test.utils";
+import Congrats from "./Congrats";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+ * Factory function to create a ShallowWrapper for the Congrats component.
+ * @function
+ * @param {object} props - Component props specific to this setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+  return shallow(<Congrats {...props} />);
+};
+
+test("should render without error", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component.length).toBe(1);
+});
+
+test("should render no text when `success` prop is false", () => {
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component.text()).toBe("");
+});
+test("should render not empty congrat message when `success` props is true", () => {
+  const wrapper = setup({ success: true });
+  const message = findByTestAttr(wrapper, "congrats-message");
+  expect(message.text().length).not.toBe(0);
+});
