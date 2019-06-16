f053bb6a41b2e343b8cb3bfb5178239b3886b07d
suchov <artem.suchov@gmail.com>
2019-06-16 23:06:47 +0300

    Add proptypes for Congrats component

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -7,6 +7,8 @@ import Congrats from "./Congrats";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const defaultProps = { success: false };
+
 /**
  * Factory function to create a ShallowWrapper for the Congrats component.
  * @function
@@ -14,11 +16,12 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
  * @returns {ShallowWrapper}
  */
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />);
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
 };
 
 test("should render without error", () => {
-  const wrapper = setup({ success: false });
+  const wrapper = setup();
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.length).toBe(1);
 });
