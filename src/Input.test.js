af17479dc93c50d7cb3dbc85fb0cfc4734dcaa7c
suchov <artem.suchov@gmail.com>
2019-06-17 16:49:43 +0300

    Set up Input component and storeFactory test utils

--- src/Input.test.js
+++ src/Input.test.js
@@ -2,11 +2,35 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
-import { findByTestAttr } from "../test/test.utils";
+import { findByTestAttr, storeFactory } from "../test/test.utils";
 import Input from "./Input";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-test('should work', () => {
-  
-})
+/**
+ * Factory function to create a ShallowWrapper for the GuessedWords component.
+ * @function setup
+ * @param {object} initialState - Initial state for this setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState);
+  const wrapper = shallow(<Input store={store} />)
+    .dive()
+    .dive();
+};
+
+describe("render", () => {
+  describe("word has not been guessed", () => {
+    test("should render component without errors", () => {});
+    test("should render input box", () => {});
+    test("should render the submit button", () => {});
+  });
+  describe("word had been guessed", () => {});
+});
+
+describe("update state", () => {
+  test("should not render component without errors", () => {});
+  test("should not render input box", () => {});
+  test("should not render the submit button", () => {});
+});
