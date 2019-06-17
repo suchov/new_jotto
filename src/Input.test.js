fff3d70dda71858a9164f9616d69cfbccdb078d7
suchov <artem.suchov@gmail.com>
2019-06-17 16:25:17 +0300

    sceleton for testing input component

---
+++ src/Input.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import { findByTestAttr } from "../test/test.utils";
+import Input from "./Input";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test('should work', () => {
+  
+})
